2273765d0e6e7f38e35be973edf25de6822211ea
Mohamed-lifa7 <mohamedlifa7@gmail.com>
2024-02-14 10:00:26 +0100

    clean code and prvoding docs

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -2,11 +2,16 @@ import crypto from "crypto";
 import { v4 as uuidv4 } from "uuid";
 
 import { db } from "@/lib/db";
-import { getVerificationTokenByEmail } from "@/data/verificiation-token";
+import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
-export const generateTwoFactorToken = async (email: string) => {
+/**
+ * Generates a two-factor authentication token for the specified email address.
+ * @param {string} email - The email address for which to generate the token.
+ * @returns {Promise<any>} - A promise that resolves with the generated token.
+ */
+export const generateTwoFactorToken = async (email: string): Promise<any> => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
   const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
@@ -31,7 +36,12 @@ export const generateTwoFactorToken = async (email: string) => {
   return twoFactorToken;
 };
 
-export const generatePasswordResetToken = async (email: string) => {
+/**
+ * Generates a password reset token for the specified email address.
+ * @param {string} email - The email address for which to generate the token.
+ * @returns {Promise<any>} - A promise that resolves with the generated token.
+ */
+export const generatePasswordResetToken = async (email: string): Promise<any> => {
   const token = uuidv4();
   const expires = new Date(new Date().getTime() + 3600 * 1000);
 
@@ -54,7 +64,12 @@ export const generatePasswordResetToken = async (email: string) => {
   return passwordResetToken;
 };
 
-export const generateVerificationToken = async (email: string) => {
+/**
+ * Generates a verification token for the specified email address.
+ * @param {string} email - The email address for which to generate the token.
+ * @returns {Promise<any>} - A promise that resolves with the generated token.
+ */
+export const generateVerificationToken = async (email: string): Promise<any> => {
   const token = uuidv4();
   const expires = new Date(new Date().getTime() + 3600 * 1000);
 
@@ -68,7 +83,7 @@ export const generateVerificationToken = async (email: string) => {
     });
   }
 
-  const verficationToken = await db.verificationToken.create({
+  const verificationToken = await db.verificationToken.create({
     data: {
       email,
       token,
@@ -76,5 +91,5 @@ export const generateVerificationToken = async (email: string) => {
     },
   });
 
-  return verficationToken;
+  return verificationToken;
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,44 @@
+import { newItemSchema } from "@/schemas";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
-export function cn(...inputs: ClassValue[]) {
+/**
+ * Combines multiple class names or class arrays into a single string.
+ * @param {...ClassValue[]} inputs - Class names or arrays of class names.
+ * @returns {string} - Combined class names as a string.
+ */
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Downloads CSV file using the provided form data and headers.
+ * @param {object} formData - Form data to convert to CSV.
+ * @param {string[]} headers - Array of header strings.
+ */
+export const downloadCSV = (formData: z.infer<typeof newItemSchema>, headers: string[]): void => {
+  const csvData = convertToCSV(formData, headers);
+  const blob = new Blob([csvData], { type: "text/csv" });
+  const url = window.URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.setAttribute("download", `${formData.title.split(" ").join("_")}.csv`);
+  document.body.appendChild(link);
+  link.click();
+};
+
+/**
+ * Converts data object into CSV format.
+ * @param {object} data - Data object to be converted into CSV.
+ * @param {string[]} headers - Array of header strings.
+ * @returns {string} - CSV formatted string.
+ */
+export const convertToCSV = (data: { [key: string]: string }, headers: string[]): string => {
+  const rows = [headers.join(",")];
+
+  const rowData = headers.map((header) => data[header]);
+  rows.push(rowData.join(","));
+
+  return rows.join("\n");
+};
